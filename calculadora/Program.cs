863db0b261cd1b271149b72655b67c201d9fd0b4
JohnLTI <xjl16@outlook.com>
2023-07-11 23:37:14 -0300

    Criação Função Subtração

--- calculadora/Program.cs
+++ calculadora/Program.cs
@@ -6,7 +6,7 @@ namespace calculadora
     {
         static void Main(string[] args)
         {
-            Soma();
+            Subtracao();
         }
 
         static void Soma()
@@ -24,6 +24,25 @@ namespace calculadora
             float resultado = v1 + v2;
             // Console.WriteLine("O resultado da soma é " + resultado);
             Console.WriteLine($"O resultado da soma é {resultado}"); //CIFRÃO PERMITE UTILIZAR CHAVES PARA INTERPORLAR VARIÁVEL AO TEXTO
+            Console.ReadKey();
+        }
+
+        static void Subtracao()
+        {
+
+            Console.Clear();
+
+            Console.WriteLine("Primeiro Valor:");
+            float v1 = float.Parse(Console.ReadLine());
+
+            Console.WriteLine("Segundo Valor:");
+            float v2 = float.Parse(Console.ReadLine());
+
+            Console.WriteLine("");
+
+            float resultado = v1 - v2;
+            Console.WriteLine($"O resultado da subtração é {resultado}");
+            Console.ReadKey();
         }
     }
 }
